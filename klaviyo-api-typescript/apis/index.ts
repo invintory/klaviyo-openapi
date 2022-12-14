6a79d1fcd68fb4e97bc3a197aa6c277b337f407c
Sam C. Finan <samcfinan@gmail.com>
2022-12-14 15:07:34 -0500

    generated typescript from openapi

---
+++ klaviyo-api-typescript/apis/index.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './CatalogsApi';
+export * from './ClientApi';
+export * from './EventsApi';
+export * from './FlowsApi';
+export * from './ListsApi';
+export * from './MetricsApi';
+export * from './ProfilesApi';
+export * from './SegmentsApi';
+export * from './TemplatesApi';
